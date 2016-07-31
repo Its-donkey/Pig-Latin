6ef369e2f9737d7b2c0f11f102031df934043e61
Jeff Shapiro <jeff@shapirohousehold.com>
2016-07-31 16:03:41 +1000

    Initial

---
+++ Pig Latin/Pig Latin/frmPigLatin.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPigLatin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtWordInput = New System.Windows.Forms.TextBox()
+        Me.txtConversionOutput = New System.Windows.Forms.TextBox()
+        Me.btnClearInputText = New System.Windows.Forms.Button()
+        Me.btnConvertText = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtWordInput
+        '
+        Me.txtWordInput.Location = New System.Drawing.Point(13, 13)
+        Me.txtWordInput.Multiline = True
+        Me.txtWordInput.Name = "txtWordInput"
+        Me.txtWordInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtWordInput.Size = New System.Drawing.Size(637, 129)
+        Me.txtWordInput.TabIndex = 0
+        Me.txtWordInput.Text = "one two three four five"
+        '
+        'txtConversionOutput
+        '
+        Me.txtConversionOutput.Location = New System.Drawing.Point(12, 339)
+        Me.txtConversionOutput.Multiline = True
+        Me.txtConversionOutput.Name = "txtConversionOutput"
+        Me.txtConversionOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtConversionOutput.Size = New System.Drawing.Size(637, 129)
+        Me.txtConversionOutput.TabIndex = 1
+        '
+        'btnClearInputText
+        '
+        Me.btnClearInputText.Location = New System.Drawing.Point(163, 221)
+        Me.btnClearInputText.Name = "btnClearInputText"
+        Me.btnClearInputText.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearInputText.TabIndex = 2
+        Me.btnClearInputText.Text = "Clear"
+        Me.btnClearInputText.UseVisualStyleBackColor = True
+        '
+        'btnConvertText
+        '
+        Me.btnConvertText.Location = New System.Drawing.Point(287, 221)
+        Me.btnConvertText.Name = "btnConvertText"
+        Me.btnConvertText.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvertText.TabIndex = 3
+        Me.btnConvertText.Text = "Convert"
+        Me.btnConvertText.UseVisualStyleBackColor = True
+        '
+        'frmPigLatin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(662, 480)
+        Me.Controls.Add(Me.btnConvertText)
+        Me.Controls.Add(Me.btnClearInputText)
+        Me.Controls.Add(Me.txtConversionOutput)
+        Me.Controls.Add(Me.txtWordInput)
+        Me.Name = "frmPigLatin"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtWordInput As TextBox
+    Friend WithEvents txtConversionOutput As TextBox
+    Friend WithEvents btnClearInputText As Button
+    Friend WithEvents btnConvertText As Button
+End Class
---
+++ Pig Latin/Pig Latin/frmPigLatin.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Text
+
+Public Class frmPigLatin
+    Private Sub btnClearInputText_Click(sender As Object, e As EventArgs) Handles btnClearInputText.Click
+        txtWordInput.Clear()
+        txtConversionOutput.Clear()
+    End Sub
+
+    Private Sub btnConvertText_Click(sender As Object, e As EventArgs) Handles btnConvertText.Click
+        Dim input As String = txtWordInput.Text
+
+        If String.IsNullOrEmpty(input) Then
+            MsgBox("No valid text has been entered to translate")
+            Exit Sub
+        Else
+            'Split text into array
+            Dim words() As String = input.Split(CType(" ", Char()))
+            Dim firstCharacter As String
+            Dim outputText As New StringBuilder
+
+            For i As Integer = 0 To words.GetUpperBound(0)
+
+                firstCharacter = words(i).First
+
+                If words(i).Length > 2 Then
+
+                    Dim wordCharacters() = words(i).ToCharArray()
+
+                    For Each s In "@#$%^&*1234567890".ToCharArray
+                        For Each wc In words(i).ToCharArray()
+                            If s = wc Then
+                                GoTo AppendText
+                            End If
+                        Next
+                    Next
+
+                    Select Case firstCharacter
+
+                        Case "a", "e", "i", "o", "u",
+                             "A", "E", "I", "O", "U"
+
+                            Dim punctuationMark As String = words(i).Last
+                            Dim attachedPunctuation As String = Nothing
+
+                            For Each p In "':,—!-().?"";".ToCharArray()
+                                If punctuationMark = p Then
+                                    words(i) = words(i).Remove(words(i).Length - 1)
+                                    attachedPunctuation = p
+                                End If
+                            Next
+
+                            words(i) = words(i) & "way" & attachedPunctuation
+
+                        Case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
+                            "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"
+                            Dim vowelFirstIndex As Integer = 999
+
+                            For Each c In "aeiouy".ToCharArray()
+                                If words(i).IndexOf(c) <> -1 And words(i).IndexOf(c) < vowelFirstIndex Then
+                                    vowelFirstIndex = words(i).IndexOf(c)
+                                End If
+                            Next
+
+                            Dim charactersToMove As String = words(i).Substring(0, vowelFirstIndex)
+
+                            Dim punctuationMark As String = words(i).Last
+                            Dim attachedPunctuation As String = Nothing
+
+                            For Each p In "':,—!-().?"";".ToCharArray()
+                                If punctuationMark = p Then
+                                    words(i) = words(i).Remove(words(i).Length - 1)
+                                    attachedPunctuation = p
+                                End If
+                            Next
+
+                            words(i) = words(i).Remove(0, vowelFirstIndex) & charactersToMove & "ay" & If(attachedPunctuation IsNot Nothing, attachedPunctuation, "")
+                    End Select
+
+                End If
+AppendText:
+                outputText.Append(words(i) & " ")
+            Next
+
+            txtConversionOutput.Text = outputText.ToString
+        End If
+    End Sub
+
+    Function getVowels() As String
+
+    End Function
+
+    Private Sub txtWordInput_KeyDown(sender As Object, e As KeyEventArgs) Handles txtWordInput.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+End Class
